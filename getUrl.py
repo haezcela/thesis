20ee7b8bf6d331180fb89cbb9bc0579be2010e1c
haezcela <aleczhysa@gmail.com>
2024-03-30 14:33:40 +0800

    Scraped cases from year 1999-2000

--- getUrl.py
+++ getUrl.py
@@ -63,7 +63,7 @@ def save_data_to_txt(data, folder, filename):
 
 # Example usage:
 base_url = "https://elibrary.judiciary.gov.ph"
-year = "2000"
+year = "1999"
 
 month_urls = generate_month_urls(base_url, year)
 
